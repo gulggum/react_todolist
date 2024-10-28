7b31bf43f8ed2122d38f9e4ebeec7b613cb97eff
gulggum <163222604+gulggum@users.noreply.github.com>
2024-10-28 12:13:14 +0900

    Update index.tsx

--- react-todolist/src/index.tsx
+++ react-todolist/src/index.tsx
@@ -2,12 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { RecoilRoot } from "recoil";
+import { BrowserRouter } from "react-router-dom";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
-  <RecoilRoot>
-    <App />
-  </RecoilRoot>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <RecoilRoot>
+      <App />
+    </RecoilRoot>
+  </BrowserRouter>
 );
